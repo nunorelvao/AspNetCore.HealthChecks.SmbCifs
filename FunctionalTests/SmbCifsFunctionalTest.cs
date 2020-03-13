0eeedad181e6c64bd64ccce76ebfd6bd072ffe7a
Nuno Relvao <nuno.relvao@misterfly.com>
2020-03-13 17:51:20 +0000

    try running functional test

---
+++ FunctionalTests/SmbCifsFunctionalTest.cs
@@ -0,0 +1,106 @@
+using FluentAssertions;
+using FunctionalTests.Base;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using AspNetCore.HealthChecks.SmbCifs.DependencyInjection;
+using Xunit;
+
+namespace FunctionalTests
+{
+    //[Collection("execution")]
+    public class FunctionalTest
+    {
+        private readonly ExecutionFixture _fixture;
+
+
+        //public FunctionalTest(ExecutionFixture fixture)
+        //{
+        //    _fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+        //}
+
+        public FunctionalTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task be_healthy_if_smbcifs_is_not_available()
+        {
+
+            var webHostBuilder = new WebHostBuilder()
+                .UseStartup<DefaultStartup>()
+                .ConfigureServices(services =>
+                {
+
+                    services.AddHealthChecks()
+                        .AddSmbCifsBasicAuth(s =>
+                        {
+                            s.Hostname = "SAMBAALPINE";
+                            s.Domain = "";
+                            s.Username = "";
+                            s.UserPassword = "";
+                        }, "smbcifshc", null, new string[] { "smbcifshc" });
+
+                })
+                .Configure(app =>
+                {
+                    app.UseHealthChecks("/health", new HealthCheckOptions()
+                    {
+                        Predicate = r => r.Tags.Contains("smbcifshc")
+                    });
+                });
+
+            var server = new TestServer(webHostBuilder);
+
+            var response = await server.CreateRequest($"/health")
+                .GetAsync();
+
+            response.StatusCode
+                .Should().Be(HttpStatusCode.OK);
+
+        }
+
+        [Fact]
+        public async Task be_unhealthy_if_smbcifs_is_not_available()
+        {
+
+            var webHostBuilder = new WebHostBuilder()
+                .UseStartup<DefaultStartup>()
+                .ConfigureServices(services =>
+                {
+
+                    services.AddHealthChecks()
+                        .AddSmbCifsBasicAuth(s =>
+                        {
+                            s.Hostname = "ERRORSERVER";
+                            s.Domain = "";
+                            s.Username = "";
+                            s.UserPassword = "";
+                        }, "smbcifshc", null, new string[] { "smbcifshc" });
+
+                })
+                .Configure(app =>
+                {
+                    app.UseHealthChecks("/health", new HealthCheckOptions()
+                    {
+                        Predicate = r => r.Tags.Contains("smbcifshc")
+                    });
+                });
+
+            var server = new TestServer(webHostBuilder);
+
+            var response = await server.CreateRequest($"/health")
+                .GetAsync();
+
+            response.StatusCode
+                .Should().Be(HttpStatusCode.ServiceUnavailable);
+
+        }
+    }
+}
---
+++ RunEnv.bat
@@ -0,0 +1,5 @@
+﻿docker-compose -f docker-compose.yml -p envsambaalpine stop
+docker-compose  -f docker-compose.yml -p envsambaalpine down
+TIMEOUT /T 1  /NOBREAK
+
+docker-compose -f docker-compose.yml -p envsambaalpine up -d --build 
